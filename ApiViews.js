fed0e7faa08750e63a6adbfc4f0ed6172cc591c2
Findlay Smith <fsmith@novarumdx.com>
2019-10-08 07:06:07 +0100

    standardise errors and add comments

--- ApiViews.js
+++ ApiViews.js
@@ -6,12 +6,9 @@ const config = require("./config");
 // express' router can be used to create views that aren't directly linked to an app
 const router = express.Router();
 
+// create a redis client and select the database
 const client = redis.createClient(config.redisConf);
 client.select(0);
-const a = client.get("a", (err, res) => {
-    return res;
-});
-console.log(client.get(a));
 
 // router.get means this view can be accessed with a GET request
 router.get("/", (req, res) => {
@@ -32,35 +29,53 @@ router.get("/current_time", (req, res) => {
 });
 
 router.get("/price", (req, res) => {
+    // check if the ticker query has been provided
     if (req.query.ticker) {
         let ticker = req.query.ticker.toUpperCase();
+        // get the value from the database
         client.get(ticker, (err, val) => {
+            // check that there is no error, and that a value was return
             if (!err && val) {
                 res.json({
                     ticker: ticker,
                     price: val
                 });
-            } else if (!val) {
+            }
+            // case when no value was found in the database
+            else if (!val) {
                 let url = `https://cloud.iexapis.com/stable/stock/${ticker}/quote?token=${config.iexPublicKey}`;
+                // request the stock price from iex
                 request(url, { json: true }, (err, result, body) => {
+                    // check that there isn't an error and that a latestPrice has been returned
                     if (!err && body.latestPrice) {
+                        // set this value in the database and send it to the client
                         client.set(ticker, body.latestPrice, "EX", 120);
                         res.json({
                             ticker: ticker,
                             price: body.latestPrice
                         });
                     } else if (!body.latestPrice) {
-                        res.status(404).send("A stock with this ticker does not exist");
+                        // if latestPrice doesn't exist - the ticker can't be found
+                        // so send a 404 not found http code
+                        res.status(404).json({
+                            error: "A stock with this ticker does not exist"
+                        });
                     } else {
                         console.log(err);
                     }
                 });
-            } else {
+            }
+            // case when a database error took place
+            else {
+                // send a 500, internal server error status code with an error message
                 console.log(err);
-                res.sendStatus(500);
+                res.status(500).json({
+                    error: "Unknown database error"
+                });
             }
         });
     } else {
+        // throw an http error (400, bad request) if the ticker was not supplied
         res.status(400).json({
             error: "no ticker name supplied"
         });
